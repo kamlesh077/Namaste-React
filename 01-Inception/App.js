a0dd57fd3a7365b6f3207c8699f0be3b93daf09b
Kamlesh077 <kamleshpune43@gmail.com>
2025-09-05 15:54:19 +0530

    first react code

---
+++ 01-Inception/App.js
@@ -0,0 +1,45 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello World From React!"
+); //{} object is used to give attributes to html tags like id or class, etc.
+
+console.log(heading); // This is object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(root);
+
+// root.render(heading);
+
+// How to create nested elements & siblings
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1>I am h1 tag</h1>
+ *      <h1>I am h1 sibling tag</h1>
+ *  </div>
+ *  <div id="child2">
+ *      <h1>I am h1 tag</h1>
+ *      <h1>I am h1 sibling tag</h1>
+ *  </div>
+ * </div>
+ *
+ * ReactElement(Object) => HTML(Browser Understands)
+ */
+
+// converts to an array to create siblings of elements
+const parent = React.createElement( "div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am h1 Tag"),
+        React.createElement("h2", {}, "I am h2 sibling Tag"),
+      ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am h1 Tag"),
+        React.createElement("h2", {}, "I am h2 sibling Tag"),
+      ]),
+]);
+
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
